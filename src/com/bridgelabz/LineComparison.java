1a8f80affbdebccdf198c8194ddbc14dba8a437a
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-03-04 21:50:12 +0530

    Added : Compare two lines based on the end points

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -20,7 +20,6 @@ public class LineComparison {
         int x2 = sc.nextInt();
         System.out.print("y2 : ");
         int y2 = sc.nextInt();
-
         System.out.println("Enter coordinates of 2nd Line");
         System.out.print("x3 : ");
         int x3 = sc.nextInt();
@@ -32,15 +31,19 @@ public class LineComparison {
         int y4 = sc.nextInt();
 
 
-        //CALCULATE LENGTH OF TWO LINES
+        // CALCULATE LENGTH OF TWO LINES
         Double length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         System.out.println("Length of first line is : " +deciForm.format(length1));
         Double length2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
         System.out.println("Length of second line is : " + deciForm.format(length2));
 
-        if(length1.equals(length2))
-            System.out.println("Both lines are equal");
+        // COMPARE BOTH LENGTH
+        int lengthCompare = length1.compareTo(length2);
+        if(lengthCompare < 0)
+            System.out.println("Line 1 is smaller than Line 2");
+        else if(lengthCompare == 0)
+            System.out.println("Both lines are same");
         else
-            System.out.println("Both lines are not equal");
+            System.out.println("Line 1 is smaller than Line 2");
     }
 }
