baf895345acfe008fd0aa0dd26cf9b7560cc41e3
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-03-04 15:56:43 +0530

    Added : Length between two cartesian coordinates

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,7 +1,26 @@
 package com.bridgelabz;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
+
+        //READ INPUT COORDINATES
+        Scanner sc = new Scanner(System.in);
+        DecimalFormat deciForm = new DecimalFormat("0.##");
+        System.out.println("Enter x1 of first coordinate");
+        double x1 = sc.nextInt();
+        System.out.println("Enter y1 of first coordinate");
+        double y1 = sc.nextInt();
+        System.out.println("Enter x2 of second coordinate");
+        double x2 = sc.nextInt();
+        System.out.println("Enter y1 of second coordinate");
+        double y2 = sc.nextInt();
+
+        //CALCULATE DISTANCE
+        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("The distance is : " + deciForm.format(distance));
     }
 }
